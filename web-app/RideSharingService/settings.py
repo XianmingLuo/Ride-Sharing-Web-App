fc6beb8dc1c772c7d59c44678a8432ef42230eb7
root <root@vcm-23974.vm.duke.edu>
2022-01-31 23:42:39 -0500

    Finalize

--- web-app/RideSharingService/settings.py
+++ web-app/RideSharingService/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'vcm-23974.vm.duke.edu',
     '127.0.0.1',
+    'web',
 ]
 
 
@@ -82,10 +83,10 @@ WSGI_APPLICATION = 'RideSharingService.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hw1db',
+        'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'xl369yl729',
-        'HOST': '127.0.0.1',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
